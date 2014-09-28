78adc4d23f999456b229403a4f729e6224360b6e
Yannis Panousis <ypanousi@cisco.com>
2014-09-28 01:05:05 +0100

    upload docs with id
    Change-Id: I8b57b2e7a544705113442343fe780241b21fbbdf

--- bin/es/es-upload.py
+++ bin/es/es-upload.py
@@ -20,9 +20,13 @@ def new_es_client(hosts):
   es_hosts = [{'host':host,'port':ES_PORT} for host in args.hosts]
   return Elasticsearch(es_hosts)
 
-def bulk_upload(es_client, json_documents, index, bulk_size=1000):
-  actions = [{'_type':'doc','_source':document} for document in json_documents]
+def bulk_upload(es_client, json_documents, index, doc_id=None, bulk_size=1000):  
+  if doc_id == None:
+    actions = [{'_type':'doc','_source':document} for document in json_documents]
+  else:
+    actions = [{'_type':'doc','_id':document[doc_id],'_source':document} for document in json_documents]
   return esh.streaming_bulk(es, actions, index=index, chunk_size=bulk_size)
+
 
 ES_PORT=9200
 
@@ -32,6 +36,7 @@ parser.add_argument('--jsondir', dest='directory', help='Directory of JSON files
 parser.add_argument('--index', dest='index', help='Index name')
 parser.add_argument('--bulksize', dest='bulksize', type=int, default=1000, help='Number of documents in each bulk upload')
 parser.add_argument('--deleteIndex', dest='deleteIndex', action='store_false', default=False, help='Delete index before uploading')
+parser.add_argument('--id', dest='id', help='Field to use as document ID')
 args = parser.parse_args()
 
 json_documents = read_jsons_from_directory(args.directory)
@@ -41,6 +46,6 @@ es = new_es_client(args.hosts)
 if args.deleteIndex:
   es.indices.delete(args.index)
 
-for ok, result in bulk_upload(es, json_documents, args.index):
+for ok, result in bulk_upload(es, json_documents, args.index, args.id):
   print ok, result
 
--- sample/es-upload-movies.sh
+++ sample/es-upload-movies.sh
@@ -3,9 +3,11 @@
 ES_PY=es-upload.py
 HOSTS=`es-cluster-ips 2`
 INDEX=movies
+ID=id
 
-$ES_PY --hosts `es-cluster-ips 2` --jsondir downloads/1900 --index movies --deleteIndex
+$ES_PY --hosts $HOSTS --jsondir downloads/1900 --index $INDEX --id $ID --deleteIndex
 
 for YEAR in `seq 1901 2020`; do
-  $ES_PY --hosts $HOSTS --jsondir downloads/$YEAR --index movies
+  $ES_PY --hosts $HOSTS --jsondir downloads/$YEAR --index $INDEX --id $ID
 done
+
